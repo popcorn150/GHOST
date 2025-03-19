6b73cd121022224eca1ade5a051a191c0ce8f779
popcorn150 <hungrymanchannel@gmail.com>
2025-03-19 04:29:52 +0100

    Add new product images and update flash sales products information

--- GhostLlc/src/pages/Store/ProductDetails.jsx
+++ GhostLlc/src/pages/Store/ProductDetails.jsx
@@ -4,12 +4,172 @@ import { useParams } from "react-router-dom"
 import NavBar from "../../components/NavBar";
 import flashSalesProducts from "./flashSalesProducts";
 import { TbRectangleVerticalFilled } from "react-icons/tb";
+import { iPhone_, NubiaGamingPhone, Pes5, Phone_cooler, ProGamerPackage1, XiaomiPad6 } from "../../utils";
 
-// const otherProducts = [
-//   {
-
-//   }
-// ]
+const otherProducts = [
+  {
+    id: 1,
+    slug: "i-phone",
+    name: "iPhones",
+    image: iPhone_,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: iPhone_,
+      },
+      {
+        id: 2,
+        img: iPhone_,
+      },
+      {
+        id: 3,
+        img: iPhone_,
+      },
+      {
+        id: 4,
+        img: iPhone_,
+      },
+    ]
+  },
+  {
+    id: 2,
+    slug: "xiaomi-pad-6",
+    name: "Xiaomi Pad6",
+    image: XiaomiPad6,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: XiaomiPad6,
+      },
+      {
+        id: 2,
+        img: XiaomiPad6,
+      },
+      {
+        id: 3,
+        img: XiaomiPad6,
+      },
+      {
+        id: 4,
+        img: XiaomiPad6,
+      },
+    ]
+  },
+  {
+    id: 3,
+    slug: "nubia-gaming-phone",
+    name: "Nubia Gaming Phone",
+    image: NubiaGamingPhone,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: NubiaGamingPhone,
+      },
+      {
+        id: 2,
+        img: NubiaGamingPhone,
+      },
+      {
+        id: 3,
+        img: NubiaGamingPhone,
+      },
+      {
+        id: 4,
+        img: NubiaGamingPhone,
+      },
+    ]
+  },
+  {
+    id: 4,
+    slug: "pes-5",
+    name: "Playstation 5",
+    image: Pes5,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: Pes5,
+      },
+      {
+        id: 2,
+        img: Pes5,
+      },
+      {
+        id: 3,
+        img: Pes5,
+      },
+      {
+        id: 4,
+        img: Pes5,
+      },
+    ]
+  },
+  {
+    id: 5,
+    slug: "phone-cooler",
+    name: "Phone Cooler",
+    image: Phone_cooler,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: Phone_cooler,
+      },
+      {
+        id: 2,
+        img: Phone_cooler,
+      },
+      {
+        id: 3,
+        img: Phone_cooler,
+      },
+      {
+        id: 4,
+        img: Phone_cooler,
+      },
+    ]
+  },
+  {
+    id: 6,
+    slug: "pro-gamer-package",
+    name: "Pro Gamer Package",
+    image: ProGamerPackage1,
+    info: "The fantastic integration of a phone grip, a stick-on and Shooting joystick in the world. Mobile Game Controller Finger Sleeves consist of 6 pieces In a pack. It is smooth, thin anti-sweat, breathable Full Touch screen with sensitive Shoot Aim Joystick Set for knives/Rules of Survival - Gray. This product is to measured as 4.92 x 3.31 x 0.2 inches",
+    views: "255K",
+    rating: 88,
+    screenShots: [
+      {
+        id: 1,
+        img: ProGamerPackage1,
+      },
+      {
+        id: 2,
+        img: ProGamerPackage1,
+      },
+      {
+        id: 3,
+        img: ProGamerPackage1,
+      },
+      {
+        id: 4,
+        img: ProGamerPackage1,
+      },
+    ]
+  },
+]
 
 
 const ProductDetails = () => {
@@ -102,14 +262,14 @@ const ProductDetails = () => {
 
             {/* Buy Now Button */}
             <button
-              onClick={() => { alert("Product added to cart!") }}
+              onClick={() => { alert(`${quantity} ${product.name} purchased successfully!`) }}
               className="bg-red-500 text-white px-6 py-3 rounded font-bold cursor-pointer">
               Buy Now
             </button>
           </div>
         </div>
 
-        <div className="mt-10">
+        <div className="w-full max-w-7xl mt-10 px-5">
           {/* Other Products */}
           <div className="w-full flex justify-start my-2 gap-2">
             <TbRectangleVerticalFilled className="text-red-500 self-center w-7 h-7" />
@@ -117,7 +277,7 @@ const ProductDetails = () => {
           </div>
 
           <div className="flex overflow-x-auto space-x-4 scrollbar-hide">
-            {flashSalesProducts.map((product, id) => (
+            {otherProducts.map((product, id) => (
               <>
                 <Link to={`/product/${product.slug}`} className="cursor-pointer">
                   <div
--- GhostLlc/src/pages/Store/flashSalesProducts.js
+++ GhostLlc/src/pages/Store/flashSalesProducts.js
@@ -16,6 +16,22 @@ import DLA8_1 from "../../assets/imgs/DLA8_1.jpg"
 import DLA8_2 from "../../assets/imgs/DLA8_2.webp"
 import DLA8_3 from "../../assets/imgs/DLA8_3.webp"
 
+import GGloves_1 from "../../assets/imgs/GameGlove1.jpg"
+import GGloves_2 from "../../assets/imgs/GameGlove2.jpg"
+import GGloves_3 from "../../assets/imgs/GameGlove3.jpeg"
+
+import Havit_Fuxi_1 from "../../assets/imgs/Havit_Fuxi-H3_1.jpg"
+import Havit_Fuxi_2 from "../../assets/imgs/Havit_Fuxi-H3_2.webp"
+import Havit_Fuxi_3 from "../../assets/imgs/Havit_Fuxi-H3_3.jpg"
+
+import iPad_Stand_1 from "../../assets/imgs/iPad_Stand_1.jpg"
+import iPad_Stand_2 from "../../assets/imgs/iPad_Stand_2.jpg"
+import iPad_Stand_3 from "../../assets/imgs/iPad_Stand_3.jpg"
+
+import iPad_1 from "../../assets/imgs/iPad_1.jpg"
+import iPad_2 from "../../assets/imgs/iPad_2.avif"
+import iPad_3 from "../../assets/imgs/iPad_3.webp"
+
 export const DAGEL_SS1 = DAGEL1
 export const DAGEL_SS2 = DAGEL2
 export const DAGEL_SS3 = DAGEL3
@@ -24,6 +40,22 @@ export const DAGEL_SS4 = DAGEL4
 export const DLA8_SS1 = DLA8_1
 export const DLA8_SS2 = DLA8_2
 export const DLA8_SS3 = DLA8_3
+
+export const GGloves_SS1 = GGloves_1
+export const GGloves_SS2 = GGloves_2
+export const GGloves_SS3 = GGloves_3
+
+export const Havit_Fuxi_SS1 = Havit_Fuxi_1
+export const Havit_Fuxi_SS2 = Havit_Fuxi_2
+export const Havit_Fuxi_SS3 = Havit_Fuxi_3
+
+export const iPad_Stand_SS1 = iPad_Stand_1
+export const iPad_Stand_SS2 = iPad_Stand_2
+export const iPad_Stand_SS3 = iPad_Stand_3
+
+export const iPad_SS1 = iPad_1
+export const iPad_SS2 = iPad_2
+export const iPad_SS3 = iPad_3
 
 const flashSalesProducts = [
     {
@@ -85,25 +117,25 @@ const flashSalesProducts = [
         slug: "gaming-gloves",
         name: "Gaming Gloves",
         image: GamingGloves,
-        info: "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.",
+        info: "Boasting a flexible and responsive design, these gloves offer an anti-slip, sweatproof, and durable solution for a wide variety of gaming applications.",
         views: "55K",
         rating: 99,
         screenShots: [
             {
                 id: 1,
-                img: DLA_8,
-            },
-            {
-                id: 2,
                 img: GamingGloves,
             },
             {
+                id: 2,
+                img: GGloves_SS1,
+            },
+            {
                 id: 3,
-                img: HavitGameNote,
+                img: GGloves_SS2,
             },
             {
                 id: 4,
-                img: DAGEL_Finger_Sleeves,
+                img: GGloves_SS3,
             },
         ]
     },
@@ -112,25 +144,25 @@ const flashSalesProducts = [
         slug: "havit-gamenote-fuxi-h3",
         name: "Havit Gamenote Fuxi-H3 Quad-Mode Gaming headphone",
         image: HavitGameNote,
-        info: "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.",
+        info: "This elegant and slim headset ensures maximum performance and comfort during those long and furious gaming sessions.",
         views: "80K",
         rating: 50,
         screenShots: [
             {
                 id: 1,
-                img: DLA_8,
-            },
-            {
-                id: 2,
-                img: GamingGloves,
-            },
-            {
-                id: 3,
                 img: HavitGameNote,
             },
             {
+                id: 2,
+                img: Havit_Fuxi_SS1,
+            },
+            {
+                id: 3,
+                img: Havit_Fuxi_SS2,
+            },
+            {
                 id: 4,
-                img: DAGEL_Finger_Sleeves,
+                img: Havit_Fuxi_SS3,
             },
         ]
     },
@@ -139,25 +171,25 @@ const flashSalesProducts = [
         slug: "ipad-stand",
         name: "iPad Stand",
         image: iPad_Stand,
-        info: "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.",
+        info: "Tablet Floor Stand, Adjustable Universal 360-degree Rotatable Metal Tablet Holder, Phone Stand for iPad/iPhoneX/iPad Pro or Other 4.7~12.9 Inch-Screen Devices.",
         views: "20K",
         rating: 10,
         screenShots: [
             {
                 id: 1,
-                img: DLA_8,
+                img: iPad_Stand,
             },
             {
                 id: 2,
-                img: GamingGloves,
+                img: iPad_Stand_SS1,
             },
             {
                 id: 3,
-                img: HavitGameNote,
+                img: iPad_Stand_SS2,
             },
             {
                 id: 4,
-                img: DAGEL_Finger_Sleeves,
+                img: iPad_Stand_SS3,
             },
         ]
     },
@@ -166,25 +198,25 @@ const flashSalesProducts = [
         slug: "iphone-ipad",
         name: "iPad",
         image: iPad_,
-        info: "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.",
+        info: "The new iPad Pro is impossibly thin, featuring outrageous performance with the Apple M4 chip, a breakthrough Ultra Retina XDR display, and superfast Wi-Fi 6E and 5G. Along with Apple Pencil Pro and Magic Keyboard, it brings endless versatility, creativity, and productivity to your fingertips.",
         views: "120K",
         rating: 70,
         screenShots: [
             {
                 id: 1,
-                img: DLA_8,
+                img: iPad_,
             },
             {
                 id: 2,
-                img: GamingGloves,
+                img: iPad_SS1,
             },
             {
                 id: 3,
-                img: HavitGameNote,
+                img: iPad_SS2,
             },
             {
                 id: 4,
-                img: DAGEL_Finger_Sleeves,
+                img: iPad_SS3,
             },
         ]
     },
